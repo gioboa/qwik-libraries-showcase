231943e13ce38ed06acc75d99715c54790661e9c
gioboa <giorgiob.boa@gmail.com>
2024-01-17 09:56:02 +0100

    chore: change metadata

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,10 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <meta
+        name="description"
+        content="A collection of web projects made with Qwik – Websites, Components, Libraries, Apps and more!"
+      />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
@@ -13,13 +12,3 @@ export default component$(() => {
     </>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
